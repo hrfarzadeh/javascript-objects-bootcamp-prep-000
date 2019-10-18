d5ad563c91ca2112dd80c1ecca7bc0cc736d4a03
Hamidreza Fakharzadeh <hrfarzadeh@gmail.com>
2019-10-18 09:50:59 +0000

    Automatically backed up by Learn

---
+++ objects.js
@@ -0,0 +1,9 @@
+var playlist={adel:"skyfall"};
+
+/*playlist={adel:"skyfall", Slowdive:"Alison", MyBloodyValentine:"Sometimes", PhilOchs:"Here's to the State of Mississippi" };*/
+  function updatePlaylist(Object, artistName, songTitle){
+    Object[artistName]=songTitle
+    return Object
+  };
+  updatePlaylist(playlist, Slowdive, Alison)
+
